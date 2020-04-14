ce3379ab79b36bd22346132684bb0a70f33de1f8
sung0000 <jpcsung@gmail.com>
2020-04-15 03:48:31 +0800

    Add FFT.py

--- FFT.py
+++ FFT.py
@@ -1,64 +1,42 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
-
 import serial
-
 import time
 
-
-Fs = 128.0;  # sampling rate
-
+Fs = 100.0;  # sampling rate
 Ts = 1.0/Fs; # sampling interval
-
-t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
-
-y = np.arange(0,1,Ts) # signal vector; create Fs samples
-
-
-n = len(y) # length of the signal
-
-k = np.arange(n)
-
-T = n/Fs
-
-frq = k/T # a vector of frequencies; two sides frequency range
-
-frq = frq[range(int(n/2))] # one side frequency range
-
+T = np.arange(0, 1, Ts)
+X = np.arange(0, 1, Ts)
+Y = np.arange(0, 1, Ts)
+Z = np.arange(0, 1, Ts)
+t = np.arange(0, 1, Ts)
 
 serdev = '/dev/ttyACM0'
-
-s = serial.Serial(serdev)
-
+s = serial.Serial(serdev, 115200)
 for x in range(0, int(Fs)):
-
     line=s.readline() # Read an echo string from K66F terminated with '\n'
-
-    # print line
-
-    y[x] = float(line)
-
-
-Y = np.fft.fft(y)/n*2 # fft computing and normalization
-
-Y = Y[range(int(n/2))] # remove the conjugate frequency parts
-
+    X[x] = float(line)
+    line=s.readline() # Read an echo string from K66F terminated with '\n'
+    Y[x] = float(line)
+    line=s.readline() # Read an echo string from K66F terminated with '\n'
+    Z[x] = float(line) #for x in range(0, int (Fs)):
+    line = s.readline()
+    t[x] = float(line)
 
 fig, ax = plt.subplots(2, 1)
 
-ax[0].plot(t,y)
-
+ax[0].plot(T, X, color = "red", linestyle = "-", label = "x")
+ax[0].plot(T, Y, color = "blue", linestyle = "-", label = "y")
+ax[0].plot(T, Z, color = "green", linestyle = "-", label = "z")
+ax[0].legend(loc = 'upper left', frameon = False)
 ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Acc Vector')
 
-ax[0].set_ylabel('Amplitude')
+ax[1].plot(T,t)
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('Tilt')
 
-ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
-
-ax[1].set_xlabel('Freq (Hz)')
-
-ax[1].set_ylabel('|Y(freq)|')
 
 plt.show()
-
 s.close()
+
